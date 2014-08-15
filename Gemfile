1ed23e909995b3ba625188f3549cfd5608b010af
Tyler Clemens <tyler.clemens@activenetwork.com>
2014-08-15 14:58:53 -0700

    initial commit with slack adapter setup

---
+++ Gemfile
@@ -0,0 +1,7 @@
+source "https://rubygems.org"
+
+gem "lita"
+
+# Slack
+gem "lita-slack", :git => 'https://github.com/tielur/lita-slack.git'
+gem "lita-slack-handler"
---
+++ lita_config.rb
@@ -0,0 +1,35 @@
+Lita.configure do |config|
+  # The name your robot will use.
+  config.robot.name = "Lita"
+
+  # The adapter you want to connect with. Make sure you've added the
+  # appropriate gem to the Gemfile.
+  config.robot.adapter = :slack
+
+  # Slack configurations
+  config.robot.mention_name = "@lita"
+  config.robot.alias = "lita"
+
+  # Adapters
+  config.adapter.username = "lita"
+  config.adapter.incoming_token = ENV["SLACK_INCOMING_TOKEN"]
+  config.adapter.team_domain = ENV["SLACK_TEAM_DOMAIN"]
+  config.adapter.incoming_url = ENV["SLACK_INCOMING_URL"]
+  config.adapter.add_mention = true
+
+  # Handlers
+  config.handlers.slack_handler.webhook_token = ENV["SLACK_WEBHOOK_TOKEN"]
+  config.handlers.slack_handler.team_domain = ENV["SLACK_TEAM_DOMAIN"]
+
+
+  # The locale code for the language to use.
+  # config.robot.locale = :en
+
+  # The severity of messages to log. Options are:
+  # :debug, :info, :warn, :error, :fatal
+  # Messages at the selected level and above will be logged.
+  config.robot.log_level = :info
+
+  config.redis.url = ENV["REDISTOGO_URL"]
+  config.http.port = ENV["PORT"]
+end
